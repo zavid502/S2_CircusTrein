4a6b32b5f75bb9eb8148441924903c17103aeeea
Zavid van den Worm <84294518+zavidvandenworm@users.noreply.github.com>
2024-03-27 14:33:32 +0100

    Delete CircusTreinBlazor directory

--- CircusTreinBlazor/DTO/AnimalPreset.cs
+++
@@ -1,9 +0,0 @@
-using CircusTrein;
-
-namespace CircusTreinBlazor.DTO;
-
-public class AnimalPreset (Animal animal, string image)
-{
-    public Animal Animal = animal;
-    public string Image = image;
-}
